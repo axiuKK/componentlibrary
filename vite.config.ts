3ad3546afb3911d7d8a869f0962ff7577fbb65f9
axiuKK <2931432685@qq.com>
2025-11-30 19:53:28 +0800

    vitest测试配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,          // 可以直接使用 test/expect
+    environment: "jsdom",   // 模拟浏览器环境
+  }
+}as any)
