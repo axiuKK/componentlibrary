dbd39785059f0308e14df201396862ae57854e72
axiuKK <2931432685@qq.com>
2025-12-20 22:34:13 +0800

    feat(vite.config): add visiable and fix pnpm build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html", // 输出文件
+      open: true, // 打包完成自动打开浏览器
+      gzipSize: true, // 显示压缩后的体积
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom", // 模拟浏览器环境
@@ -15,7 +23,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: path.resolve(__dirname, "src/index.tsx"),
       name: "MyUI",
       fileName: "my-ui",
     },
